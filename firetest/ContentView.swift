d84dd4fce05fe07a85b3627c6aefcb51f514c492
Faysal Mahmud <pi.faysal.mahmud@gmail.com>
2024-12-05 01:34:54 +0600

    Initial Commit

---
+++ firetest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firetest
+//
+//  Created by Faysal Mahmud on 5/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firetest/firetestApp.swift
@@ -0,0 +1,17 @@
+//
+//  firetestApp.swift
+//  firetest
+//
+//  Created by Faysal Mahmud on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct firetestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
